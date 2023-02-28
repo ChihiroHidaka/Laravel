5cb0b7e2fbf1d91db2c747a1e5fd744b109db3ba
chihiro hidaka <chihi.stu270@gmail.com>
2023-02-28 22:29:30 +0000

    08課題

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,9 @@ Route::get('/', function () {
 });
 
 use App\Http\Controllers\Admin\NewsController;
-Route::controller(NewsController::class)->prefix('admin')->group(function() {
-    Route::get('news/create', 'add')->middleware('auth');
+Route::controller(NewsController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function(){
+    Route::get('news/create','add')->name('news.add');
+    Route::post('news/create','create')->name('news.create');
 });
 
 
@@ -29,9 +30,11 @@ Route::controller(AAAController::class)->prefix('admin')->group(function() {
 });
 
 use App\Http\Controllers\Admin\ProfileController;
-Route::controller(ProfileController::class)->prefix('admin')->group(function() {
-    Route::get('profile/create ', 'add')->middleware('auth');
+Route::controller(ProfileController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function() {
+    Route::get('profile/create ', 'add')->name("profile.create");
+    Route::post('profile/create ', 'create')->name("profile.create");
     Route::get('profile/edit', 'edit')->middleware('auth');
+    Route::post('profile/create ', 'edit')->name("profile.update");
 });
 
 
