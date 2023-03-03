bf547993675925fb63696ffbd9063deb8bf95cb1
chihiro hidaka <chihi.stu270@gmail.com>
2023-03-03 03:26:43 +0000

    ０９課題途中

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Profile;
+
+
 class ProfileController extends Controller
 {
     //
@@ -16,6 +19,23 @@ class ProfileController extends Controller
     
     public function create(Request $request)
     {
+    
+    
+        $this->validate($request,Profiles::$rules);
+
+        $profiles = new Profile;
+        $form = $request->all();
+
+
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        // フォームから送信されてきたimageを削除する
+        unset($form['image']);
+
+        // データベースに保存する
+        $profiles->fill($form);
+        $profiles->save();
+
         return redirect('admin/profile/create');
         
     }
